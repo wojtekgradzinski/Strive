7ba0786df7af8c4c06e61072dbeffcb18bc80479
Carlos-Trebbau <ctrebbau@pm.me>
2021-05-30 13:20:50 +0200

    Adds test module and similar_char fun

--- IntroPython/String_and_Files/Strings_and_Files6,3.py
+++ IntroPython/String_and_Files/Strings_and_Files6,3.py
@@ -14,3 +14,24 @@ def index_vowels(text):
         if f_text[i] == 'a' or f_text[i] == 'e' or f_text[i] == 'i' or f_text[i] == 'o' or f_text[i] == 'u':
             d[c] = i
     return d
+
+def similar_char(s1, s2):
+    """
+    String, String -> ListOfString
+    >>> similar_char("The holy Grail", "Life of Brian")
+    ['o in position 5', 'a in position 11']
+    """
+    los = []
+    str1, str2 = s1.casefold(), s2.casefold()
+    if len(str1) <= len(str2):
+        shorter_str, longer_str = str1, str2
+    else:
+        shortest_str, longer_str = str2, str1
+    for i, c in enumerate(shortest_str):
+        if shortest_str[i] == longer_str[i]:
+            los.append(str(c) + " in position " + str(i))
+    return los
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod(verbose=True))
